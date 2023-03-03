ad253bf6fe2c7509acc4f4c875eea32294534b2e
Carolina Bastos <carol1999b@gmail.com>
2023-03-03 11:59:03 +0000

    Add VariasStrings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string normalString = "Hello world!\n";
+            string normalString2 = "\"\tHello world!\n\"";
+            string verbatimString = @"Hello world!\n";
+            string verbatimString2 = @"""\tHello world!\n""";
+            string normalSquare = "\u25A1";
+            string verbatimCircle = @"\u25CB";
+
+
+            Console.WriteLine(normalString);
+            Console.WriteLine(normalString2);
+            Console.WriteLine(verbatimString);
+            Console.WriteLine(verbatimString2);
+            Console.WriteLine(normalSquare);
+            Console.WriteLine(verbatimCircle);
+        }
+    }
+}
