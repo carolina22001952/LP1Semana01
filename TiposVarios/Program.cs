290b10b5d66104c5d470cc1f2544c61930e66f06
Carolina Bastos <carol1999b@gmail.com>
2023-03-03 11:12:28 +0000

    Add Floating Point types

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -6,7 +6,7 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
-            // Integral type variables.
+            // Integral types variables.
             int a = 12;
             short b = 50;
             long c = 10L;
@@ -15,12 +15,19 @@ namespace TiposVarios
             // Print variables.
             Console.WriteLine($"{a}\n{b}\n{c}\n{d}");
 
-            // Char variables   
+            // Char variables.  
             char square = '\u25A1';
             char circle = '\u25CB';
 
-            // Print chars
+            // Print chars.
             Console.WriteLine($"\n{square}\n{circle}");
+
+            // Floating Point types variables
+            float e = 1.234f;
+            double f = 4.321;
+
+            // Print variables.
+            Console.WriteLine($"\n{e}\n{f}");
         }
     }
 }
