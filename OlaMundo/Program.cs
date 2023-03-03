30a79cfa4fd7f03d40f72554fb044d0d7ed9067a
Carolina Bastos <carol1999b@gmail.com>
2023-03-03 10:29:27 +0000

    Add comments to class Program

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,8 +2,15 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// Class Program.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// This method is responsible for starting the program.
+        /// </summary>
+        /// <param name="args"> Arguments given in the console.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
