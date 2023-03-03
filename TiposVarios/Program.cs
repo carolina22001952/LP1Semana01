c96977aedd900238d4115f74ae6f8dc7e8bb4e69
Carolina Bastos <carol1999b@gmail.com>
2023-03-03 11:01:55 +0000

    Add char variables

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -6,13 +6,21 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
-
+            // Integral type variables.
             int a = 12;
             short b = 50;
             long c = 10L;
             ulong d = 30UL;
 
+            // Print variables.
             Console.WriteLine($"{a}\n{b}\n{c}\n{d}");
+
+            // Char variables   
+            char square = '\u25A1';
+            char circle = '\u25CB';
+
+            // Print chars
+            Console.WriteLine($"\n{square}\n{circle}");
         }
     }
 }
