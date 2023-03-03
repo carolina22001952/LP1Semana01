fb5c8628f557610af9fe8d2379643d2e14f487b0
Carolina Bastos <carol1999b@gmail.com>
2023-03-03 10:51:23 +0000

    Add TiposVarios

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 12;
+            short b = 50;
+            long c = 10L;
+            ulong d = 30UL;
+
+            Console.WriteLine($"{a}\n{b}\n{c}\n{d}");
+        }
+    }
+}
